dc740b6e01f43487802592bf247dbc41049828c4
Jermaine Murray <jmurray3@terpmail.umd.edu>
2022-10-30 21:43:04 -0400

    goodbye function has been added

---
+++ simple_social_network.py
@@ -0,0 +1,11 @@
+""" Python file to print "hello" for git excercise """
+
+def print_func():
+    print("Hello, this is a test, if you see it, it passed!")
+
+print_func()
+
+def goodbye_func():
+    print("Goodbye!")
+
+goodbye_func()
